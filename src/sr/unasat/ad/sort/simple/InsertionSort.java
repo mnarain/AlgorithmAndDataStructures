33b55d89aae88be8d86ae3d207c41616a208bef0
Maarten Narain <maartennarain@hotmail.com>
2018-06-16 12:40:18 -0300

    selection and insert sort added

--- src/sr/unasat/ad/sort/simple/InsertionSort.java
+++ src/sr/unasat/ad/sort/simple/InsertionSort.java
@@ -2,6 +2,29 @@ package sr.unasat.ad.sort.simple;
 
 public class InsertionSort {
 
+    /*
+      Time Complexity: O(n^2)
+      Auxiliary Space: O(1)
+      Boundary Cases: Insertion sort takes maximum time
+      to sort if elements are sorted in reverse order.
+      And it takes minimum time (Order of n)
+      when elements are already sorted.
+     */
+    void sort(int[] array) {
+        int size = array.length;
+        for (int i = 1; i < size; ++i) {
+            int key = array[i];
+            int j = i - 1;
 
+            /* Move elements of array[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
 
 }
--- src/sr/unasat/ad/sort/simple/SelectionSort.java
+++ src/sr/unasat/ad/sort/simple/SelectionSort.java
@@ -2,4 +2,38 @@ package sr.unasat.ad.sort.simple;
 
 public class SelectionSort {
 
+    /*
+    Time Complexity: O(n^2) as there are two nested loops.
+    Auxiliary Space: O(1)
+    The good thing about selection sort is it never makes
+    more than O(n) swaps and can be useful
+    when memory write is a costly operation.
+    */
+
+    static void sort(int[] array) {
+        int size = array.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < size - 1; i++) {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < size; j++)
+                if (array[j] < array[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            //swap(array, min_idx, i);
+            int temp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = temp;
+        }
+    }
+
+/*    private static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }*/
+
 }
