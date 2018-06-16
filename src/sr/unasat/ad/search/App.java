7092a29f273c77c60b42770e20b2599f5a40d5e2
Maarten Narain <maartennarain@hotmail.com>
2018-06-16 05:46:19 -0300

    Concepts introduced: Arrays, searches, simple and advanced sorting Chapters introduced: 2,3,7

--- src/sr/unasat/ad/search/App.java
+++ src/sr/unasat/ad/search/App.java
@@ -3,12 +3,13 @@ package sr.unasat.ad.search;
 public class App {
 
     public static void main(String[] args) {
-        int[] dataSet = {12,3,7,20,4,3};
-        int termFoundAtIndex = SearchService.linearySearch(-2, dataSet);
+        int[] dataSet = {12, 3, 7, 20, 4, 3};
 
-        if (termFoundAtIndex >= 0){
-        System.out.println("De index waar de zoekterm voorkomt is " + termFoundAtIndex);
-    }else{
+        int termFoundAtIndex = SearchService.linearSearch(-2, dataSet);
+
+        if (termFoundAtIndex >= 0) {
+            System.out.println("De index waar de zoekterm voorkomt is " + termFoundAtIndex);
+        } else {
             System.out.println("De zoekterm komt niet voor in de dataset");
         }
 
