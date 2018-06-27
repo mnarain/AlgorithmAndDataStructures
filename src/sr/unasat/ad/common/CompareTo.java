67fbfd6d58aabfae591983690def839dfbdf7443
Maarten Narain <maartennarain@hotmail.com>
2018-06-27 20:24:39 -0300

    herschreven compareto method voor int comparison

--- src/sr/unasat/ad/common/CompareTo.java
+++ src/sr/unasat/ad/common/CompareTo.java
@@ -3,8 +3,9 @@ package sr.unasat.ad.common;
 public class CompareTo {
     public static int execute(int value1, int value2) {
         if (value1 != value2) {
-            return (value1 > value2) ? -1 : 1;
+            return (value1 > value2) ? 1 : -1;
         }
         return 0;
     }
+
 }
