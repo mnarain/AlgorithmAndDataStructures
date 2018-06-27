b5b01eb9563d1c0a8e7f3d4be3d548da66afce9b
Maarten Narain <maartennarain@hotmail.com>
2018-06-27 20:22:23 -0300

    herschreven compareto method voor int comparison

--- src/sr/unasat/ad/common/CompareTo.java
+++ src/sr/unasat/ad/common/CompareTo.java
@@ -7,31 +7,4 @@ public class CompareTo {
         }
         return 0;
     }
-
-
-    public static int execute(String value1, String value2) {
-
-        if (!value1.equals(value2)) {
-            int shortestStringLength = (value1.length() < value2.length()) ? value1.length() : value2.length();
-
-            //value1 eerste karakter kleiner value2 eerste karakter
-            for (int i = 0; i < shortestStringLength; i++) {
-                if (value1.charAt(i) != value2.charAt(i)) {
-                    return (value1.charAt(i) < value2.charAt(i)) ? 1 : -1;
-                    //value1 eerste karakter groter value2 eerste karakter
-                }
-            }
-            //karakter reeeks hetzelfde, values niet dezelfde lengte (vb scenario: jan, jandino)
-            return (value1.length() < value2.length()) ? 1 : -1;
-        }
-
-        // value1 equals value2
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(execute("Jan", "Jan"));
-        System.out.println(execute("Jan", "Jandino"));
-        System.out.println(execute("Jan", "Anna"));
-    }
 }
