88effa045bda896c86e78d7d1069156954eea1c6
Maarten Narain <maartennarain@hotmail.com>
2018-06-27 21:27:18 -0300

    methode 1 & 2

--- src/sr/unasat/ad/common/CompareTo.java
+++ src/sr/unasat/ad/common/CompareTo.java
@@ -12,11 +12,18 @@ public class CompareTo {
         if (!value1.equals(value2)) {
             int shortestStringLength = (value1.length() < value2.length()) ? value1.length() : value2.length();
             for (int i = 0; i < shortestStringLength; i++) { //evalueer op basis van de grote van de character
+                /*
+                Methode 1
                 if (value1.charAt(i) > value2.charAt(i)) {
                     return 1;
                 }
                 if (value1.charAt(i) < value2.charAt(i)) {
                     return -1;
+                }*/
+
+                //Methode 2
+                if (value1.charAt(i) != value2.charAt(i)) {
+                    return (value1.charAt(i) > value2.charAt(i)) ? 1 : -1;
                 }
             }
             return (value1.length() > value2.length()) ? 1 : -1; //evalueer op basis van string length
